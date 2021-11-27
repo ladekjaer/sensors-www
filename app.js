ddbc41c35dd17cd21f32466825cf3f6242626db9
Rasmus Ladekjær Pedersen <ladekjaer@gmail.com>
2021-11-27 22:49:58 +0100

    Clean up code. Grouping the code.

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const { Pool, Query } = require('pg')
-require('dotenv').config()
 const argon2 = require('argon2')
 const session = require('express-session')
 const sessionStore = require('connect-pg-simple')
-const pgSession = sessionStore(session)
 
+
+/**
+ * -------------- GENERAL SETUP -----------------
+ */
+
+require('dotenv').config()
 const SESSION_SECRET = 'my very secret string'
 const PORT = process.env.PORT
 const pool = new Pool()
 
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
+
+
+/**
+ * -------------- SESSION SETUP -----------------
+ */
 app.use(session({
 	secret: SESSION_SECRET,
 	resave: false, // if unmodified sessions should be rewritten
@@ -27,18 +37,10 @@ app.use(session({
 	}
 }))
 
-app.set('view engine', 'ejs')
 
-const checkAuthentication = (req, res, next) => {
-	if (!req.session || !req.session.authenticated) {
-		res.render('pages/login', {
-			message: `To ${req.method} ${req.path} you need to login.`
-		})
-	} else {
-		next()
-	}
-}
-
+/**
+ * -------------- ROUTES ------------------------
+ */
 app.get('/', (req, res) => {
 	res.render('pages/index')
 })
@@ -89,78 +91,20 @@ app.post('/add_user', checkAuthentication, (req, res) => {
 		})
 })
 
-const roleConvert = role => {
-	return (role === 'admin') ? 1 : 2
-}
-
-const addUser = (email, phone, role, hashedPassword, callback) => {
-	let role_id = roleConvert(role)
-	const query = {
-		text: `	insert into users
-				(email, phone, role_id, password)
-				values
-				($1::text, $2::text, $3::integer, $4::text)
-				RETURNING user_id`,
-		values: [email, phone, role_id, hashedPassword]
-	}
-	pool.query(query, (err, result) => {
-		if (err) {
-			console.log(err)
-			return callback(err)
-		}
-		return callback(null, result.rows[0].user_id)
-	})
-}
-
-let validateUser = (email, password, callback) => {
-	const query = {
-		text: `	select
-				    u.user_id
-				    , u.email
-				    , u.phone
-				    , r.role
-				    , u.password
-				from
-				    users u
-				    left outer join roles r on u.role_id = r.role_id
-				where
-				    email =  $1::text;`,
-		values: [email]
-	}
-	pool.query(query, (err, result) => {
-		if (err) {
-			console.error(err)
-			return callback(err)
-		}
-
-		let user = result.rows[0]
-
-		argon2.verify(user.password, password).then(correct => {
-			if (correct) {
-				delete user.password
-				console.log(user)
-				return callback(null, user)
-			} else {
-				return callback(null, null)
-			}
-		})
-	})
-}
-
-const getHashedPassword = async password => {
-	let hash
-	try {
-		hash = await argon2.hash(password)
-	} catch (err) {
-		console.error(err)
-		return err
-	}
-	return hash
-}
-
 app.get('/latest(/:count)?', checkAuthentication, (req, res) => {
 	let count = req.params.count || 1
-	pool.query('select * from temperature_view order by capture_time desc limit $1::integer', [count], (err, result) => {
+	const query = {
+		text: `
+			SELECT
+				*
+			FROM
+				temperature_view
+			ORDER BY
+				capture_time DESC
+			LIMIT $1::integer;`,
+		values: [count]
+	}
+	pool.query(query, (err, result) => {
 		res.writeHead(200, {
 			'content-type': 'text/plain; charset=utf8'
 		})
@@ -198,6 +142,100 @@ app.get('/data(/:count)?', checkAuthentication, (req, res) => {
 		res.send(thermdata)
 	})
 })
+
+
+/**
+ * -------------- DATABASE ----------------------
+ */
+
+const addUser = (email, phone, role, hashedPassword, callback) => {
+	let role_id = roleConvert(role)
+	const query = {
+		text: `	insert into users
+				(email, phone, role_id, password)
+				values
+				($1::text, $2::text, $3::integer, $4::text)
+				RETURNING user_id`,
+		values: [email, phone, role_id, hashedPassword]
+	}
+	pool.query(query, (err, result) => {
+		if (err) {
+			console.log(err)
+			return callback(err)
+		}
+		return callback(null, result.rows[0].user_id)
+	})
+}
+
+const validateUser = (email, password, callback) => {
+	const query = {
+		text: `	select
+				    u.user_id
+				    , u.email
+				    , u.phone
+				    , r.role
+				    , u.password
+				from
+				    users u
+				    left outer join roles r on u.role_id = r.role_id
+				where
+				    email =  $1::text;`,
+		values: [email]
+	}
+	pool.query(query, (err, result) => {
+		if (err) {
+			console.error(err)
+			return callback(err)
+		}
+
+		let user = result.rows[0]
+
+		argon2.verify(user.password, password).then(correct => {
+			if (correct) {
+				delete user.password
+				console.log(user)
+				return callback(null, user)
+			} else {
+				return callback(null, null)
+			}
+		})
+	})
+}
+
+
+/**
+ * -------------- HELPER FUNCTIONS --------------
+ */
+
+function checkAuthentication(req, res, next) {
+	if (!req.session || !req.session.authenticated) {
+		res.render('pages/login', {
+			message: `To ${req.method} ${req.path} you need to login.`
+		})
+	} else {
+		next()
+	}
+}
+
+async function getHashedPassword(password) {
+	let hash
+	try {
+		hash = await argon2.hash(password)
+	} catch (err) {
+		console.error(err)
+		return err
+	}
+	return hash
+}
+
+function roleConvert(role) {
+	return (role === 'admin') ? 1 : 2
+}
+
+
+/**
+ * -------------- SERVER ------------------------
+ */
 
 app.listen(PORT, () => {
 	console.log(`HTTP server listening at http://localhost:${PORT}`)
